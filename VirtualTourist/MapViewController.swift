998840cbac8bd1e944b811610e049c66f895c3e1
kellyi <kinnes@gmail.com>
2015-07-29 20:35:31 -0400

    feat: Add edit button actions
    - toggle rightbarbuttonitem title
    - show/hide "Tap Pins to Delete" UIView

--- VirtualTourist/MapViewController.swift
+++ VirtualTourist/MapViewController.swift
@@ -13,19 +13,37 @@ class MapViewController: UIViewController, MKMapViewDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    @IBOutlet weak var tapPinsNotificationView: UIView!
+    
     // MARK: - Setup UIViews
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        let editButton = UIBarButtonItem(title: "Edit", style: .Plain, target: self, action: "edit")
+        var editButton = UIBarButtonItem(title: "Edit", style: .Plain, target: self, action: "editButtonPressed")
         self.navigationItem.rightBarButtonItem = editButton
         self.title = "VirtualTourist"
+        tapPinsNotificationView.backgroundColor = UIColor.redColor()
+        tapPinsNotificationView.hidden = true
     }
     
-    func edit() {
-        println("Hello World!")
+    func editButtonPressed() {
+        if self.navigationItem.rightBarButtonItem?.title == "Edit" {
+            self.navigationItem.rightBarButtonItem?.title = "Done"
+            edit()
+        } else {
+            self.navigationItem.rightBarButtonItem?.title = "Edit"
+            done()
+        }
     }
 
-
+    func edit() {
+        tapPinsNotificationView.hidden = false
+        println("edit")
+    }
+    
+    func done() {
+        tapPinsNotificationView.hidden = true
+        println("done")
+    }
 }
 
