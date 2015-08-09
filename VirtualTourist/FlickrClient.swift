64fcf931951122a07ac08de778a32dcc8d21593a
Kelly Innes <kinnes@gmail.com>
2015-08-09 15:16:17 -0400

    chore: Setup FlickrClient class

---
+++ VirtualTourist/FlickrClient.swift
@@ -0,0 +1,13 @@
+//
+//  FlickrClient.swift
+//  VirtualTourist
+//
+//  Created by Kelly Innes on 8/9/15.
+//  Copyright (c) 2015 Kelly Innes. All rights reserved.
+//
+
+import Foundation
+
+class FlickrClient : NSObject {
+
+}
---
+++ VirtualTourist/FlickrConstants.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrConstants.swift
+//  VirtualTourist
+//
+//  Created by Kelly Innes on 8/9/15.
+//  Copyright (c) 2015 Kelly Innes. All rights reserved.
+//
+
+import Foundation
+
+extension FlickrClient {
+    
+    struct Constants {
+        static let ApiKey : String = "ba5e187d0f24ac42ae4d3bbfd49e58c7"
+        static let Secret : String = "c079388f775acaaa"
+    }
+}
